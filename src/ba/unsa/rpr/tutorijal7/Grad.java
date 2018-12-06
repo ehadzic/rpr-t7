171f1e23b54eb6cbe416f45381e1d39016f059fe
ehadzic <44297122+ehadzic@users.noreply.github.com>
2018-12-06 16:31:36 +0100

    main

--- src/ba/unsa/rpr/tutorijal7/Grad.java
+++ src/ba/unsa/rpr/tutorijal7/Grad.java
@@ -1,4 +1,44 @@
 package ba.unsa.rpr.tutorijal7;
 
-public class Grad {
+import java.io.Serializable;
+
+public class Grad implements Serializable {
+    private String naziv;
+    private int brojStanovnika;
+    private double[] temperature = new double[1000];
+
+
+    public Grad() {
+
+    }
+
+    public Grad(String naziv,double[] temperature,int brojStanovnika ) {
+        this.naziv=naziv;
+        this.brojStanovnika = brojStanovnika;
+        this.temperature=temperature;
+    }
+
+    public String getNaziv() {
+        return naziv;
+    }
+
+    public int getBrojStanovnika() {
+        return brojStanovnika;
+    }
+
+    public double[] getTemperature() {
+        return temperature;
+    }
+
+    public void setNaziv(String naziv) {
+        this.naziv = naziv;
+    }
+
+    public void setBrojStanovnika(int brojStanovnika) {
+        this.brojStanovnika = brojStanovnika;
+    }
+
+    public void setTemperature(double[] temperature) {
+        this.temperature = temperature;
+    }
 }
